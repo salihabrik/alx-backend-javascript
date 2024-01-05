b04f42165d87137a5f2843b948dabad6b60bd85a
salihabrik <briksaliha@gmail.com>
2024-01-05 12:34:14 +0100

    update

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,8 +1,8 @@
 export default function signUpUser(firstName, lastName) {
     const myPromise = new Promise((resolve) => {
-      resolve(
+    resolve(
         { firstName, lastName },
-      );
+    );
     });
     return myPromise;
-  }
+}
