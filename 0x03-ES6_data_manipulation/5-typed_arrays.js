99b9c68b6691fcff0c82ecc72955d8debb842655
salihabrik <briksaliha@gmail.com>
2024-01-08 18:56:13 +0000

    typed Arrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,14 @@
+function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const dataView = new DataView(buffer);
+
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  dataView.setInt8(position, value);
+
+  return dataView;
+}
+
+export default createInt8TypedArray;
