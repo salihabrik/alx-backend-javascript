7ed410ef20582616d782804a4261891f98b7a0e7
salihabrik <briksaliha@gmail.com>
2024-01-09 13:22:35 +0100

    clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+const cleanSet = (set, startString) => {
+    const filteredValues = Array.from(set)
+      .filter(value => value.startsWith(startString))
+      .map(value => value.slice(startString.length));
+  
+    return filteredValues.join('-');
+  };
+  
+  export default cleanSet;
+  
