13aa6b47efa6abd8bb0eb27846468cc88382ac63
salihabrik <briksaliha@gmail.com>
2024-01-09 12:47:19 +0000

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
-const cleanSet = (set, startString) => {
-  const filteredValues = Array.from(set)
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length));
+export default function cleanSet(set, startString) {
+  // Filter set values that start with startString
+  const filteredValues = [...set].filter(value => value.startsWith(startString));
 
-  return filteredValues.join('-');
-};
+  // Append the rest of each value and join with "-"
+  const resultString = filteredValues.map(value => value.slice(startString.length)).join('-');
 
-export default cleanSet;
+  return resultString;
+}
