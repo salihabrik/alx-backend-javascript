fd09161feb5803fb04878f6e6c301a3726aac246
salihabrik <briksaliha@gmail.com>
2024-01-09 12:27:31 +0000

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,9 @@
 const cleanSet = (set, startString) => {
-    const filteredValues = Array.from(set)
-      .filter(value => value.startsWith(startString))
-      .map(value => value.slice(startString.length));
-  
-    return filteredValues.join('-');
-  };
-  
-  export default cleanSet;
-  
+  const filteredValues = Array.from(set)
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length));
+
+  return filteredValues.join('-');
+};
+
+export default cleanSet;
