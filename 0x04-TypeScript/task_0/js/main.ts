55a730e8b2bca3b7c6fdbd6ffebda289255a020b
salihabrik <briksaliha@gmail.com>
2024-01-10 23:26:14 +0100

    Creating an interface for a student

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -6,10 +6,10 @@ interface Student {
 }
 
 const student1: Student = {
-    firstName: 'John',
-    lastName: 'Doe',
+    firstName: 'Sa',
+    lastName: 'bri',
     age: 22,
-    location: 'London',
+    location: 'Algeria',
 };
 
 const student2: Student = {
@@ -20,3 +20,22 @@ const student2: Student = {
 };
 
 const studentsList: Student[] = [student1, student2];
+const table: HTMLTableElement = document.createElement('table');
+const tbody: HTMLTableSectionElement = document.createElement('tbody');
+
+studentsList.forEach((student) => {
+    const row: HTMLTableRowElement = document.createElement('tr');
+    const firstNameCell: HTMLTableCellElement = document.createElement('td');
+    const locationCell: HTMLTableCellElement = document.createElement('td');
+
+    firstNameCell.textContent = student.firstName;
+    locationCell.textContent = student.location;
+
+    row.appendChild(firstNameCell);
+    row.appendChild(locationCell);
+
+    tbody.appendChild(row);
+});
+
+table.appendChild(tbody);
+document.body.appendChild(table);
