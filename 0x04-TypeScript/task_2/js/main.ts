44905fd1b15cf9bc5287dfcb8aae9e3739e17581
salihabrik <briksaliha@gmail.com>
2024-01-12 21:18:59 +0100

    Creating functions specific to employees

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -54,4 +54,21 @@ interface DirectorInterface {
   console.log(createEmployee(200)); // Output: Teacher
   console.log(createEmployee(1000)); // Output: Director
   console.log(createEmployee('$500')); // Output: Director
+
+// Function isDirector
+function isDirector(employee: Director | Teacher): employee is Director {
+    return (employee as Director).workDirectorTasks !== undefined;
+  }
   
+  // Function executeWork
+  function executeWork(employee: Director | Teacher): string {
+    if (isDirector(employee)) {
+      return employee.workDirectorTasks();
+    } else {
+      return employee.workTeacherTasks();
+    }
+  }
+  
+  // Testing executeWork function
+  console.log(executeWork(createEmployee(200))); // Output: Getting to work
+  console.log(executeWork(createEmployee(1000))); // Output: Getting to director tasks
