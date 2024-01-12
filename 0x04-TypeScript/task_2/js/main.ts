9e29cac0555e376a1639ab8198aac025c4469817
salihabrik <briksaliha@gmail.com>
2024-01-12 21:24:43 +0100

    String literal types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -72,3 +72,22 @@ function isDirector(employee: Director | Teacher): employee is Director {
   // Testing executeWork function
   console.log(executeWork(createEmployee(200))); // Output: Getting to work
   console.log(executeWork(createEmployee(1000))); // Output: Getting to director tasks
+
+// String literal type named Subjects
+type Subjects = 'Math' | 'History';
+
+// Function named teachClass
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else if (todayClass === 'History') {
+    return 'Teaching History';
+  } else {
+    // Handle other subjects if needed
+    return 'Not teaching any specific subject';
+  }
+}
+
+// Testing teachClass function
+console.log(teachClass('Math')); // Output: Teaching Math
+console.log(teachClass('History')); // Output: Teaching History
